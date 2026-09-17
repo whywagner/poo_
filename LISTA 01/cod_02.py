efc04f2b8026ad36c5c52d756511b7a398355c61
Yurnax <wagnerben800@gmail.com>
2026-09-17 13:10:29 -0300

    Delete LISTA 01/cod_02.py

--- LISTA 01/cod_02.py
+++
@@ -1,9 +0,0 @@
-velocidade_Media = 30 
-
-tempo_horas = float(input("Digite o tempo de deslocamento (em horas): "))
-
-tempo_segundos = tempo_horas * 3600
-distancia_km = velocidade_Media * tempo_segundos
-distancia_metros = distancia_km * 1000
-
-print(f"A distância total percorrida foi de {distancia_metros} metros")
